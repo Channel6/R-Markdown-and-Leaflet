2cb58df9202e37028721f7c0529ae5a4ddbee87e
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-05-13 15:25:54 -0700

    fix typos

--- Leaflet.R
+++ Leaflet.R
@@ -9,10 +9,8 @@ CFCULogo <- makeIcon(
 )
 
 CFCUlocations <- data.frame(
-	lat = c(41.872693, 38.453594, 38.502237, 38.306802, 38.611669, 38.404080, 38.446126)
+	lat = c(41.872693, 38.453594, 38.502237, 38.306802, 38.611669, 38.404080, 38.446126),
 	lng = c(-88.099606, -122.771138, -122.997407, -122.302803, -122.871419, -122.825748, -122.730874)
 )
 
-my_map <- leaflet() %>% addTiles() 
-my_map <- my_map %>% addMarkers(lat=43.1594, lng= -79.2469, popup="Branch location", icon = ciscoIcon)
-my_map
+CFCUlocations %>% leaflet() %>% addTiles() %>% addMarkers(popup="Branch location", icon = CFCULogo)
